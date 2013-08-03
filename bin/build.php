192f2d46040f686b1868ece29be73fcbbc98c30f
Franck Magnan <fmagnan@gmail.com>
2013-08-03 22:01:39 +0200

    initialise resume

---
+++ bin/build.php
@@ -0,0 +1,28 @@
+#!/usr/bin/php
+
+<?php
+
+$projectRootPath = __DIR__ . '/../';
+
+require $projectRootPath . 'vendor/autoload.php';
+
+function importData($path)
+{
+    return require $path . 'inc/config.inc.php';
+}
+
+function getView($path)
+{
+    $loader = new Twig_Loader_Filesystem($path . 'views');
+    $view = new Twig_Environment($loader, array(
+        'auto_reload' => true,
+        'cache' => $path . 'cache',
+    ));
+    return $view;
+}
+
+$data = importData($projectRootPath);
+$view = getView($projectRootPath);
+$resume = $view->render('main.twig', $data);
+
+file_put_contents($projectRootPath . 'index.html', $resume);
---
+++ inc/config.inc.php
@@ -0,0 +1,31 @@
+<?php
+
+return array(
+    'bio' => array(
+        'firstname' => 'Franck',
+        'lastname' => 'Magnan',
+        'jobTitle' => 'Lead Web Developer'
+    ),
+    'info' => array(
+        'email' => 'fmagnan@gamelab.fr',
+        'website' => 'fmagnan.gamelab.fr',
+        'linkedin' => 'http://fr.linkedin.com/pub/franck-magnan/53/b43/38a',
+        'github' => 'https://github.com/fmagnan',
+    ),
+    'experiences' => array(
+        'mediastay' => array(
+            'date' => array(
+                'begin' => 'Novembre 2011',
+                'end' => '-',
+            ),
+            'jobTitle' => 'Lead PHP Developer',
+            'company' => 'Mediastay',
+            'companyWebsite' => 'http://www.mediastay.com',
+            'content' => 'Récupération du code existant des loteries en ligne Kingoloto, Bananalotto et Luckysurf
+                | Amélioration continue et remaniement du code
+                | Mise en place de composer, migration de php 5.2 en php 5.4
+                | Optimisation des performances (frontend et backend)
+                | Animation de l\'équipe (3 personnes)'
+        )
+    )
+);
