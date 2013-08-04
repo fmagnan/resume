f16523bdf9faa0106b8b701cd8edafb321a74717
Franck Magnan <fmagnan@gmail.com>
2013-08-04 23:01:29 +0200

    refactor experiences

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -3,7 +3,7 @@
 return array(
     'bio' => array(
         'firstname' => 'Franck',
-        'jobTitle' => 'Lead Web Developer',
+        'jobTitle' => 'Lead Developer / Architecte PHP',
         'lastname' => 'Magnan',
         'place' => 'Lyon',
         'summary' => ''
@@ -25,14 +25,34 @@ return array(
                 'duration' => '',
                 'end' => ''
             ),
-            'description' => 'Après avoir débuté',
             'place' => 'Lyon',
             'tasks' => array(
-                'Récupération du code existant des loteries en ligne Kingoloto, Bananalotto et Luckysurf',
-                'Amélioration continue et remaniement du code',
-                'Mise en place de composer, migration de php 5.2 en php 5.4',
-                'Optimisation des performances (frontend et backend)',
-                'Animation de l\'équipe (3 personnes)'
+                'publishing' => array(
+                    'description' => 'Lead developer du pôle publishing',
+                    'subtasks' => array(
+                        'Récupération du code existant des loteries en ligne Kingoloto, Bananalotto et Luckysurf',
+                        'Mise en place de composer, migration de php 5.2 en php 5.4',
+                        'Optimisation des performances (frontend et backend)',
+                        'Animation de l\'équipe (3 personnes)',
+                        'Réalisation d\'un moteur de génération de sites de concours'
+                    )
+                ),
+                'gamespassport' => array(
+                    'description' => 'Optimisation de la partie cliente du site gamespassport',
+                    'subtasks' => array(
+                        'audit du code, optimisation et remaniement du code',
+                        'diminution du temps de chargement d\'une page de l\'ordre de 60%',
+                    )
+                ),
+                'monetisation' => array(
+                    'description' => 'Développeur backend sur MdsMatch, AdServer propriétaire de Mediastay',
+                    'subtasks' => array(
+                        'Découverte et appropriation du code en complète autonomie',
+                        'Conception et développement de nouvelles fonctionnalités',
+                        'Correction de bugs, d\'anomalies et optimisation du code',
+                        'Mise en place de tests unitaires et de l\'intégration continue (Jenkins)'
+                    )
+                )
             ),
             'title' => 'Lead PHP Developer',
         ),
@@ -44,9 +64,72 @@ return array(
                 'duration' => '8 ans',
                 'end' => 'octobre 2011'
             ),
-            'description' => '',
             'place' => 'Villeurbanne',
-            'tasks' => array(),
+            'tasks' => array(
+                'php' => array(
+                    'description' => 'Travail au forfait au sein du pôle PHP / Tierce Maintenance Applicative',
+                    'subtasks' => array(
+                        'Audit de code',
+                        'Soutien auprès des développeurs juniors'
+                    )
+                ),
+                'drupal' => array(
+                    'description' => 'Réalisation du site de La Provence',
+                    'subtasks' => array(
+                        'Migration iso-fonctionnalités du site basé sur la solution propriétaire
+                        NStein vers une solution open source basée sur Drupal 6',
+                        'Développement de modules Drupal (backoffice et frontoffice)',
+                        'Responsable de la reprise des données du site existant'
+                    )
+                ),
+                'ezpublish' => array(
+                    'description' => 'Travaux autour de la solution logicielle eZ Publish',
+                    'subtasks' => array(
+                        'Migration du code et des données du site intranet de Laforêt Immobilier (eZ Publish3.9.1 vers 4.3.0).',
+                        'Développement d\'un module de synchronisation entre le système d\'information de PremiereVision et le site.',
+                        'Interfaçage entre l\'application iphone Fly\'on dédiée à l\'affichage du tableau des vols et le site des
+                        aéroports de Lyon.',
+                        'Reprise du site existant Madmagz et amélioration de l\'interface utilisateur (javascript).',
+                        'Implémentation d\'une recherche à facettes pour le site Banque du fleuve Rhône patrimoine.',
+                        'Enrichissement du modèle de données du site Mémoire des Célestins.',
+                    )
+                ),
+                'typo3' => array(
+                    'description' => 'Travaux autour de la solution logicielle Typo3',
+                    'subtasks' => array(
+                        'Reprise du site éditorial de FlashInfos. Amélioration du système de création et d\'envoi
+                        de newsletters (Typo3 + PHPLIST)',
+                        'Réalisation de 3 sites extranet destinés à des clientèles différentes d\'une mutuelle
+                        et disposant d\'une interface d\'administration commune',
+                        'Création de modules Typo3 variés : thèmes graphiques, import et export des données,
+                        interface administrateur, interface utilisateur',
+                        'Formation auprès des utilisateurs (éditeurs et administrateurs)',
+                        'Prise en charge de la tierce maintenance applicative (correction des anomalies, chiffrage et
+                        développement de nouvelles fonctionnalités)'
+                    )
+                ),
+                'ruby' => array(
+                    'description' => 'Dans le cadre de la refonte de l\'outil de gestion de tickets Xguard,
+                    adaptation de la solution logicielle Redmine',
+                    'subtasks' => array(
+                        'Autoformation sur le langage Ruby, le framework Ruby on Rails et le logiciel Redmine',
+                        'Développement d\'extensions visant à gérer des objets génériques (création, modification,
+                        suppression) et les liens qui les unissent ("appartient", "contient", "hérite" ...)'
+                    )
+                ),
+                'java' => array(
+                    'description' => 'Travaux en java',
+                    'subtasks' => array(
+                        'Réalisation d\'un module de manipulation d\'image (redimensionnement et rognage)
+                     en Java, Jsp et Javascript',
+                        'Migration d\'une application web de suivi de projets commerciaux depuis un framework
+                        propriétaire Java vers Struts',
+                        'Portage d\'une application équivalente à 15 années/homme (300 000 lignes)',
+                        'Mise en place d\'une démarche agile (eXtreme Programming) incluant travail en binôme, métrique
+                        quotidienne, feedback rapide, développement incrémental, hiérarchisation des tâches'
+                    )
+                ),
+            ),
             'title' => 'Ingénieur d\'études open source',
         ),
         'cirad' => array(
@@ -57,9 +140,17 @@ return array(
                 'duration' => '16 mois',
                 'end' => 'janvier 2002'
             ),
-            'description' => 'Service national en tant que volontaire à l\'aide technique',
             'place' => 'Guadeloupe',
-            'tasks' => array(),
+            'tasks' => array(
+                'vat' => array(
+                    'description' => 'Service national en tant que volontaire à l\'aide technique',
+                    'subtasks' => array(
+                        'Administration du réseau, des serveurs, de la messagerie, des sauvegardes (Linux, HP-UX, Novell)',
+                        'Assistance à 80 utilisateurs (sur site et par téléphone) : tout niveau de gravité',
+                        'Maintenance matérielle sur les postes de travail (Windows 95, 98 et 2000)'
+                    )
+                )
+            ),
             'title' => 'Responsable du parc informatique',
         )
     ),
@@ -108,132 +199,3 @@ return array(
     )
 );
 
-
-/*
- *
- * Avril 2003 / Octobre 2011 – 8 ans : Ingénieur d'études open source
-Open Wide Outsourcing (2 mois) – Adaptation de la solution logicielle Redmine
-•
-•
-•
-•
-Dans le cadre de la refonte de l'outil de gestion de tickets Xguard,
-Développement d'extensions pour la solution logicielle de gestion de projets Redmine
-Autoformation sur le langage Ruby, le framework Ruby on Rails et le logiciel Redmine
-Développement d'une extension permettant de gérer les annonces Redmine dans un contexte global
-aussi bien que dans un contexte projet
-• Développement d'une extension permettant de gérer des objets génériques (création, modification,
-suppression) et les liens qui les unissent ("appartient", "contient", "hérite" ...)
-Environnement technique : Redmine, Ruby, Linux, Apache, PostgreSQL et Javascript
-Open Wide – Travaux autour de la solution eZ Publish
-•
-•
-•
-•
-•
-•
-•
-•
-Migration du site intranet eZ Publish (3.9.1 vers 4.3.0 ) de Laforêt Immobilier
-• récupération des fichiers sources et de la base de données du site existant
-• passage de la version 4 de php à la version 5, migration de la base de données
-• correction des anomalies, réécriture de certaines extensions, correction d'erreurs de conception
-Réalisation de nouveaux types de workflows, de nouveaux types de données
-Refonte du site internet PremiereVision
-Mise en place d'une interface entre le site des aéroports de Lyon et l'application pour iphone Fly'on
-dédiée à l'affichage des informations de départ et d'arrivée des vols ainsi qu'à la disponibilité des places
-de parking.
-Développement de nouveaux handlers permettant de gérer la connexion des utilisateurs, d'ajouter des
-tâches à la création ou à la mise à jour des contenus
-Création de différentes extensions (par exemple : interfaçage avec le logiciel de gestion publicitaire
-OpenX)
-Réalisation des sites Open Wide
-Maintenance et évolution du site Madmagz de création de magazine en ligne•
-Configuration et personnalisation de l'extension ezfind avec gestion des facets et des filtres de recherche
-pour le site 'Banque du fleuve Rhône patrimoine'
-• Réalisation du back office et du front office du portail intranet du Ministère de la culture
-Environnement technique : eZ Publish, Linux, Apache, MySQL et Javascript
-Groupe Hersant Media (4 mois) – Réalisation du site de La Provence
-•
-Migration iso-fonctionnalités du site existant basé sur la solution propriétaire NStein vers une solution
-open source basée sur Drupal 6
-• Développement de modules Drupal aussi bien au niveau du backoffice que du frontoffice
-• Mise en place de la reprise des données du site existant
-Environnement technique : Drupal 6, Windows, Linux, Apache, MySQL et Javascript
-Open Wide (6 mois) – Réalisations autour des sites internet de la Ville de Lyon
-•
-Réalisation de sites ou modules dans le cadre du projet de maintenance évolution des sites internet de la
-ville de lyon (par exemple, le portail mes démarches)
-Environnement technique : Zope, Plone, python
-Open Wide (2 mois) – Réalisation site http://os4i.com (inactif)
-• Conception du site internet http://os4i.com
-• Installation du socle technique
-• Paramétrage de la solution et développement de modules
-Environnement technique : Zope, Plone et Python
-Mutuelle Fraternelle d’Assurance (2 mois) – Mise en place d’un intranet collaboratif
-•
-Intégration du thème de la M.F.A. et adaptation d’Ovidentia (éditeur de texte riche, menu déroulant
-dynamique, formulaires)
-• Formateur auprès des administrateurs et utilisateurs finaux
-Environnement technique : Ovidentia, Linux, Apache, MySQL, Php et Javascript
-Open Wide – Réalisation de divers travaux Web
-•
-•
-•
-•
-•
-Gestion au quotidien du site web d’Open Wide (Typo3)
-Réalisation de modules pour le site intranet d’Open Wide (Typo3)
-Participation aux sites web de Teaser et d’Accelance MSP
-Refonte du site statique de l’Institut Paul Delouvrier à l’aide de l’outil de gestion de contenus Drupal
-Réalisation d’un module de manipulation d’image (redimensionnement et rognage) en Java, Jsp et
-Javascript
-• Reprise du site éditorial de FlashInfos en tierce maintenance applicative
-• Récupération de la solution applicative de création d'éditions et d'envoi de newsletters basée sur Typo3
-et PHPLIST
-• Correction d'anomalies, développement d'extensions backend et frontend
-Environnement technique : Drupal, Typo3, Linux, Apache, MySQL, Php et Javascript
-April Solutions (10 mois) – Réalisation d’un extranet métier Assurance
-•
-•
-•
-Environnement de développement LAMP (Linux, Apache, MySQL, Php) avec l’outil Typo3
-Mise en place d’une interface de gestion des contrats d’assurance en ligne
-Réalisation de 3 sites extranet destinés à des clientèles différentes et disposant d’une interface
-d’administration commune
-• Création de modules Typo3 (thèmes graphiques, import et export des données, interface administrateur,
-interface utilisateur)
-• Formation à l’administration fonctionnelle - éditeur (Typo3)
-• Formation à l’extension et à l’administration technique (Typo3)
-• Prise en charge de la tierce maintenance applicative (correction des anomalies, chiffrage et
-développement de nouvelles fonctionnalités)
-Environnement technique : Typo3, Linux, Apache, MySQL, Php et Javascript
-Mutuelle des motards (4 mois) – Réalisation des modules d’éditique du système d’information
-• Environnement de développement JAVA (SWT/JFace)
-• Création d’un client lourd de gestion de modèles de documents destinés au publipostage de masse
-• Intégration du module de publipostage dans l’application principale de gestion des sociétaires
-• Interfaçage de Microsoft Word et Adobe Acrobat Reader avec l’application JAVA
-Environnement technique : JAVA, SWT, Jface, Acrobat ReaderLe Grand Lyon (9 mois) – Création du portail économique de l’agglomération lyonnaise
-• Environnement de développement JAVA/J2EE (solution logicielle Red Hat CCM)
-• Participation à la réalisation d’un portail Web d’agrégation et de gestion de contenus
-• Modification et adaptation de l’interface d’administration du portail
-• Travail au sein d'une équipe de 7 personnes (Open Wide et Sopra Group)
-Environnement technique : JAVA/J2EE, Red Hat CCM
-Thales Information System (10 mois) – mise en œuvre d’un prototype de portail JAVA/J2EE unique et
-sécurisé
-• Démonstration de la faisabilité technique (proof of concept)
-• Mise en place d'un prototype basé sur une architecture distribuée n-tiers
-• Utilisation de la plate-forme de développement Red Hat-CCM 6.0
-• Intégration des composants propriétaires Oracle 9i et SiteMinder
-• Enrichissement du framework avec des modules spécifiquement développés pour Thales
-• Rédaction de documentations (guides pratiques, manuels d'installation et d'utilisation)
-Environnement technique : Red Hat-CCM 6.0, Oracle 9i, SiteMinder, framework
-Thales Communication (5 mois) – migration JAVA/J2EE
-•
-Migration d’une application web de suivi de projets commerciaux depuis un framework propriétaire
-Java vers Struts
-• Portage d’une application équivalente à 15 années/homme (300 000 lignes)
-• Mise en place d'une démarche agile (eXtreme Programming) incluant travail en binôme, métrique
-quotidienne, feedback rapide, développement incrémental, hiérarchisation des tâches
-Environnement technique : framework, Struts, Java
- */
